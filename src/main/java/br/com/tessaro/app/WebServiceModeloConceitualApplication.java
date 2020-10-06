bd43608c64ca11733ac0603ebf733eb09b87f254
Cassio Tessaro <cassioate@gmail.com>
2020-10-05 21:38:05 -0300

    Projeto Criado

---
+++ src/main/java/br/com/tessaro/app/WebServiceModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package br.com.tessaro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceModeloConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tessaro/app/WebServiceModeloConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tessaro.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceModeloConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
